aab3469cd5fce09480061a6965fe83d60551008d
Silva <d.barbosa.da.silva@accenture.com>
2021-06-14 10:29:55 -0300

    Finalizando projeto 8-rest-to-api-graphql

--- 8-rest-to-api-graphql/src/data/data-drivers.ts
+++ 8-rest-to-api-graphql/src/data/data-drivers.ts
@@ -1,5 +1,6 @@
 import { checkYear, roundCheck } from '../lib/utils';
 import { F1 } from './data-source';
+import { paginationOptions } from '../lib/utils';
 
 export class DriversData extends F1 {
     constructor(){
@@ -12,10 +13,8 @@ export class DriversData extends F1 {
                 cacheOptions: { ttl: 60 }
             });
         }
-        const offset = (page - 1) * pageElements;
-        const limit = pageElements;
-        const filter = `limit=${ limit }&offset=${ offset }`;
-        return await this.get(`drivers.json?${ filter }`, {
+
+        return await this.get(`drivers.json?${ paginationOptions(pageElements, page) }`, {
             cacheOptions: { ttl: 60 }
         });
     }
@@ -37,6 +36,13 @@ export class DriversData extends F1 {
 
     async getDriver(id: string){
         return await this.get(`drivers/${id}.json`,
+            { cacheOptions: { ttl: 60 } }
+        );
+    }
+
+    async getSeasonsPilotsRanking(year: string){
+        year = checkYear(year);
+        return await this.get(String(year).concat('/driverStandings.json'),
             { cacheOptions: { ttl: 60 } }
         );
     }
